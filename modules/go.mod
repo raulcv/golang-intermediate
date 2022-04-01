5ec57f5178d574b43171c5353da5f003dd202302
Raul <iraulcv@gmail.com>
2022-04-01 02:02:17 -0500

    using modules

---
+++ modules/go.mod
@@ -0,0 +1,7 @@
+module github.com/raulcv/modules
+
+go 1.18
+
+require github.com/donvito/hellomod v1.0.1
+
+require github.com/donvito/hellomod/v2 v2.0.0 // indirect
---
+++ modules/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"github.com/donvito/hellomod"
+	hellomodv2 "github.com/donvito/hellomod/v2"
+)
+
+func main() {
+	hellomod.SayHello()
+	hellomodv2.SayHello("OMG	")
+}
