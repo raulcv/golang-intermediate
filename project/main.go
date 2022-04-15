cbcb787a77046c790b3562c91da82adc41d7f1f8
Raul <iraulcv@gmail.com>
2022-04-15 03:55:41 -0500

    Workers, pools, dispacher and more

---
+++ project/main.go
@@ -0,0 +1,137 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+// Workers, Pools, Dispatcher
+type Job struct {
+	Name   string
+	Delay  time.Duration
+	Number int
+}
+type Worker struct {
+	Id         int
+	JobQueue   chan Job
+	WorkerPool chan chan Job
+	QuitChan   chan bool
+}
+type Dispatcher struct {
+	WorkerPool chan chan Job
+	MaxWorkers int
+	JobQueue   chan Job //Job queue - cola de trabajo
+}
+
+func NewWorker(id int, WorkerPool chan chan Job) *Worker {
+	return &Worker{
+		Id:         id,
+		JobQueue:   make(chan Job),
+		WorkerPool: WorkerPool,
+		QuitChan:   make(chan bool),
+	}
+}
+
+func (w Worker) Start() {
+	go func() {
+		for {
+			w.WorkerPool <- w.JobQueue
+			select {
+			case job := <-w.JobQueue:
+				fmt.Printf("Worker with Id %d started\n", w.Id)
+				fib := Fibonacci(job.Number)
+				time.Sleep(job.Delay)
+				fmt.Printf("Worker with Id %d Finished with result %d\n", w.Id, fib)
+			case <-w.QuitChan:
+				fmt.Printf("Worker with Id %d Stopped\n", w.Id)
+			}
+		}
+	}()
+}
+
+func (w Worker) Stop() {
+	go func() {
+		w.QuitChan <- true
+	}()
+}
+
+func Fibonacci(n int) int {
+	if n <= 1 {
+		return 1
+	}
+	return Fibonacci(n-1) + Fibonacci(n-2)
+}
+
+func NewDispatcher(jobQueue chan Job, maxWorkers int) *Dispatcher {
+	worker := make(chan chan Job, maxWorkers)
+	return &Dispatcher{
+		JobQueue:   jobQueue,
+		MaxWorkers: maxWorkers,
+		WorkerPool: worker,
+	}
+}
+
+func (d *Dispatcher) Dispatch() {
+	for {
+		select {
+		case job := <-d.JobQueue:
+			go func() {
+				workerJobQueue := <-d.WorkerPool
+				workerJobQueue <- job
+			}()
+		}
+	}
+}
+
+func (d *Dispatcher) Run() {
+	for i := 0; i < d.MaxWorkers; i++ {
+		worker := NewWorker(i, d.WorkerPool)
+		worker.Start()
+	}
+	go d.Dispatch()
+}
+func RequestHandler(w http.ResponseWriter, r *http.Request, jobQueue chan Job) {
+	if r.Method != "POST" {
+		w.Header().Set("Allow", "POST")
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+	// fmt.Println(r.Method)
+	delay, err := time.ParseDuration(r.FormValue("delay"))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	value, err := strconv.Atoi(r.FormValue("value"))
+	if err != nil {
+		http.Error(w, "Invalid value", http.StatusBadRequest)
+		return
+	}
+	name := r.FormValue("name")
+	if name == "" {
+		http.Error(w, "Invalid name", http.StatusBadRequest)
+		return
+	}
+	job := Job{Name: name, Delay: delay, Number: value}
+	jobQueue <- job
+	w.WriteHeader(http.StatusCreated)
+}
+func main() {
+	const (
+		maxWorkers   = 4
+		maxQueueSize = 20
+		port         = ":8081"
+	)
+	jobQueue := make(chan Job, maxQueueSize)
+	dispatcher := NewDispatcher(jobQueue, maxWorkers)
+	dispatcher.Run()
+	//Http://localhost:8081/fib
+	http.HandleFunc("/fib", func(w http.ResponseWriter, r *http.Request) {
+		RequestHandler(w, r, jobQueue)
+	})
+	log.Fatal(http.ListenAndServe(port, nil))
+}
+
+// Create web server to process jobs
